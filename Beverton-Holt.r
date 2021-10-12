2a90597aaa0c92ad60c745651d3753cbadaed664
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2021-10-12 16:39:46 -0300

    Update Beverton-Holt.r
    Agregado el caso para K aleatorio con distribución normal

--- Beverton-Holt.r
+++ Beverton-Holt.r
@@ -1,10 +1,15 @@
-# Librerías necesarias
+# 1 - Librerías necesarias
 install.packages("ggplot2")
 library(ggplot2)
 
-# Definición de la función
+# 2 - Definición de la función
+#     R  = índice de proliferación
+#     K  = capacidad de carga del entorno
+#     N0 = población en el tiempo inicial
+#     Tf = tiempo final (puede ser cualquier valor entero)
 BevertonHolt = function(R, K, N0, Tf){
   poblacion = numeric()
+
   for(t in 0:Tf){
     Nt = (K * N0)/(N0 + (K - N0)* exp(-R*t)) 
     poblacion <- c(poblacion, Nt) 
@@ -13,53 +18,31 @@ BevertonHolt = function(R, K, N0, Tf){
   return(poblacion)
 }
 
-# Prueba de la función con R = 0.5, K = 500, población inicial de 2 individuos
-evolucion <- BevertonHolt(0.5, 500, 2, 100)
+# 3 - Prueba de la función con R = 0.5, K = 500, población inicial de 2 individuos
+evolucion_temporal <- BevertonHolt(0.5, 500, 2, 100)
 tiempo = 0:100
 
-# Creación de un dataframe
-grafico <- data.frame(evolucion, tiempo)
+# 4 - Creación de un dataframe para almacenar 
+grafico <- data.frame(evolucion_temporal, tiempo)
 
-# Gráfico del comportamiento de la población
+# 5- Gráfico del comportamiento de la población
 comportamiento <- ggplot(grafico, 
                          aes(x = tiempo, 
-                             y = evolucion)) + 
+                             y = evolucion_temporal)) + 
                   geom_point() + 
                   labs(title = "Comportamiento temporal de la población", 
                            x = "Tiempo", 
                            y = "Tamaño de la población")
 
+# 6 - En caso de que K sea aleatoria y siga una distribución normal con media y desvío conocidos, puede adaptarse así:
+BevertonHolt = function(R, N0, Tf){
+  poblacion = numeric()
 
+  for(i in 1:Tf){
+    K <- rnorm(1, Media, DesvíoEstándar)
+    Nt = (K * N0)/(N0 + (K - N0)* exp(-R*i)) 
+    poblacion <- c(poblacion, Nt) 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return(poblacion)
+}
